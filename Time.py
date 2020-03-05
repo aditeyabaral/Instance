cf45d64bea3ea25d6a4ad8611c21add6071a7349
Aditeya Baral <33230959+aditeyabaral@users.noreply.github.com>
2020-03-05 11:58:15 +0530

    Delete Time.py

--- Time.py
+++
@@ -1,4 +0,0 @@
-from datetime import datetime
-s1 =  str(datetime.now().hour) +':'+ str(datetime.now().minute)+':'+str(datetime.now().second)
-s2 = str(datetime.now().day) + '/' + str(datetime.now().month) +  '/' + str(datetime.now().year)
-print s1 + ' , ' + s2
